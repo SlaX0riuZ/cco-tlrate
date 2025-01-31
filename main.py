2330f83f15c445ece61b0181e6dc4ca73fc54c16
SlaX0riuZ <slaxoriuz@gmail.com>
2025-01-31 10:41:26 -0600

    initializing main window

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import messagebox
+
+winmain = tk.Tk()
+winmain.title('CCO-matrate')
+winmain.geometry('300x300')
+winmain.config(bg='#8FD032')
+winmain.resizable(False, False)
+winmain.iconbitmap('greencube.ico')
+
+winmain.mainloop()
