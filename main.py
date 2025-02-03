e6bf653623f911a284c0bf1bc96a9a80aaff047a
SlaX0riuZ <slaxoriuz@gmail.com>
2025-02-03 11:36:54 -0600

    end period

--- main.py
+++ main.py
@@ -13,23 +13,22 @@ App Favicon
 Graphing of data
 Extra Credit Awarded for Creativity
 '''
-# Input: [Bsidebool, Slatedbool, combinedholeytier, combinedpizzatier] so 4 buttons
-# /////////////////////////////// Initialize Window
+# /////////////////////////////// [INITIALIZE WINDOW]
 winmain = tk.Tk()
 winmain.title('Tally Laborer - Input')
 winmain.geometry('300x300')
 winmain.config(bg='#8FD032')
 winmain.resizable(False, False)
 winmain.iconbitmap('greencube.ico')
-# /////////////////////////////// Initialize Variables
+# /////////////////////////////// [INITIALIZE VARIABLES]
 bsidevar = tk.IntVar()
-slatedvar = tk.IntVar()
+slatedvar = tk.IntVar() # Checkboxes can only manipulate .IntVar variable types
 nfile = open('fixeddata.txt', 'a') # nfile used to append to fixeddata
-# ///////////////////////////////
+# /////////////////////////////// [COMMANDS]
 def senddata():
-    bbool = '0'
+    bbool = '0' # Temporary variables; should be 0 each time senddata() is used
     sbool = '0'
-    if str(bsidevar) == 'PY_VAR1':
+    if str(bsidevar) == 'PY_VAR1': # Both if statements are used to convert the .IntVar string to a numerical one
         bbool = '1'
     if str(slatedvar) == 'PY_VAR1':
         sbool = '1'
@@ -38,57 +37,67 @@ def senddata():
     nfile.write('\n') # newline
     nfile.write(bbool + '/' + sbool + '/' + htier + '/' + ptier) # data storage format
 
-def clearallconfirm():
+def clearallconfirm(): # function to open confirmation window
+    # Initialize window "winconfirm" from winmain
     winconfirm = tk.Toplevel(winmain)
     winconfirm.geometry('200x200')
     winconfirm.config(bg='#61A53F')
     winconfirm.title('CONFIRM DATA ERASURE')
     winconfirm.iconbitmap('greencube.ico')
+    # Respective commands for "YES" click and "NO" click
     def wcdestroy():
         winconfirm.destroy()
     def wcconfirm():
-        open('fixeddata.txt','w').write('')
+        open('fixeddata.txt','w').write('') # Clearing the file
         wcdestroy()
+    # Setup confirmation label and buttons
     tk.Label(winconfirm, text='Are you sure you want to \n erase all data to file? \n This includes all previous entries.').place(x=10, y=10)
     tk.Button(winconfirm, text='NO', command=wcdestroy).place(x=50,y=100)
     tk.Button(winconfirm, text='YES', command=wcconfirm).place(x=120,y=100)
 
-def graphdata():
+def graphdata(): # function to graph the data
+    # Initialize window "wingraph" from winmain
     wingraph = tk.Toplevel(winmain)
     wingraph.geometry('500x500')
     wingraph.title('GRAPHED OUTPUT')
     wingraph.iconbitmap('greencube.ico')
     wingraph.config(bg='#61A53F')
-# ///////////////////////////////
+# /////////////////////////////// [WINDOW SETUP]
+# Respective title label
 tk.Label(winmain, text='Tally Laborer Calculator for CCO').place(x=10,y=10)
-
+# Checkbox and label setup for B-Side Boolean
 tk.Label(winmain, text='Check box if Collector\'s is B-Side.',relief='raised').place(x=10,y=70,height=25)
 bside_cb = tk.Checkbutton(winmain, text='B-Side?', variable=bsidevar, onvalue=1, offvalue=0)
 bside_cb.config(bg='#61A53F',selectcolor='#C4F129', relief='raised')
 bside_cb.place(x=200,y=70)
-
+# Checkbox and label setup for Slated Boolean
 tk.Label(winmain, text='Check box if Collector\'s is Slated.',relief='raised').place(x=10,y=100,height=25)
 slated_cb = tk.Checkbutton(winmain, text='Slated?', variable=slatedvar, onvalue=1, offvalue=0)
 slated_cb.config(bg='#61A53F',selectcolor='#C4F129', relief='raised')
 slated_cb.place(x=200,y=100)
-
+# Label and respective input for Holey Sigils
 tk.Label(winmain, text='Input Combined Tier of Holeys: ',relief='raised').place(x=10,y=140)
 holey_txt = tk.Entry(winmain,relief='raised')
 holey_txt.place(x=200,y=140,width=90)
-
+# Label and respective input for Pizza Sigils
 tk.Label(winmain, text='Input Combined Tier of Pizzas: ',relief='raised').place(x=10,y=170)
 pizza_txt = tk.Entry(winmain,relief='raised')
 pizza_txt.place(x=200,y=170,width=90)
-
+# Sending Data Button
 send_bn = tk.Button(winmain,relief='raised',text='Store Inputs',command=senddata)
-send_bn.place(x=50,y=200)
-
+send_bn.place(x=10,y=200)
+# Clearing File Button
 clearfile_bn = tk.Button(winmain,relief='raised',text='CLEAR ALL DATA',command=clearallconfirm)
-clearfile_bn.place(x=150,y=200)
-
+clearfile_bn.place(x=90,y=200)
+# Graphing Button
 graphdata_bn = tk.Button(winmain,relief='raised',text='Graph All Data',command=graphdata)
 graphdata_bn.place(x=100,y=240)
-
+# Error Handling Label
+err_lbl = tk.Label(winmain,text='ERROR HANDLER')
+err_lbl.place(x=10,y=270,width=280)
+# /////////////////////////////// [UPDATES AND LOOPS]
+# Update the Checkboxes using .flash()
 bside_cb.flash()
 slated_cb.flash()
+# Mainloop
 winmain.mainloop()
